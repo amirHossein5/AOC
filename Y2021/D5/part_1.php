32d9b9fae621c22530bf07133a382ba9f78680bc
amir <adada5116@gmail.com>
2022-08-21 20:56:34 +0430

    D5 P1

---
+++ Y2021/D5/part_1.php
@@ -0,0 +1,91 @@
+<?php
+
+require __DIR__.'/../../vendor/autoload.php';
+require __DIR__.'/helpers.php';
+
+use Classes\Read;
+
+$puzzle = Read::lineByline(__DIR__.'/puzzle.txt');
+
+$allCoordinates = [];
+$maxNumber = 0;
+
+foreach ($puzzle as $coordinates) {
+    preg_match('/^(\d+),(\d+) -> (\d+),(\d+)$/', $coordinates, $numbers);
+    array_splice($numbers, 0, 1);
+
+    foreach ($numbers as $number) {
+        $maxNumber > $number ?: $maxNumber = $number;
+    }
+
+    // just those coordinates that are vertically or horizontally
+    if ($numbers[0] === $numbers[2] or $numbers[1] === $numbers[3]) {
+        // ordering
+        if ($numbers[0] < $numbers[2] or $numbers[1] < $numbers[3]) {
+            $coordinates = getCoordinatesArray($numbers[0], $numbers[1], $numbers[2], $numbers[3]);
+        } else {
+            $coordinates = getCoordinatesArray($numbers[2], $numbers[3], $numbers[0], $numbers[1]);
+        }
+
+        /** between cordinates */
+        // horizontally-> 0,9->5,9
+        if ($coordinates[2]['x'] - $coordinates[1]['x'] > 0) {
+            $minusResult = $coordinates[2]['x'] - $coordinates[1]['x'];
+
+            for ($i = 0; $i <= $minusResult; $i++) {
+                $allCoordinates[] = [
+                    'x' => $coordinates[1]['x'] + $i,
+                    'y' => $coordinates[1]['y']
+                ];
+            }
+        } else {
+            $minusResult = $coordinates[2]['y'] - $coordinates[1]['y'];
+
+            for ($i = 0; $i <= $minusResult; $i++) {
+                $allCoordinates[] = [
+                    'x' => $coordinates[1]['x'],
+                    'y' => $coordinates[1]['y'] + $i
+                ];
+            }
+        }
+    }
+}
+
+/** draw vanilla . diagram */
+file_put_contents(__DIR__.'/diagram.txt', '');
+
+// append horizontally
+for ($i = 0; $i <= $maxNumber; $i++) {
+    file_put_contents(__DIR__.'/diagram.txt', file_get_contents(__DIR__.'/diagram.txt') . '.');
+}
+// append vertically
+$appendLine = explode(PHP_EOL, file_get_contents(__DIR__.'/diagram.txt'))[0];
+
+for ($i = 0; $i < $maxNumber; $i++) {
+    $diagramFile = fopen(__DIR__.'/diagram.txt', 'a');
+    fwrite($diagramFile, PHP_EOL.$appendLine);
+    fclose($diagramFile);
+}
+
+/** draw numbers */
+$leastTwoLineOverlapCount = 0;
+$diagram = Read::contents(__DIR__.'/diagram.txt');
+
+foreach ($allCoordinates as $coordinates) {
+    $x = $coordinates['x'];
+    $y = $coordinates['y'];
+
+    $row = str_split(Read::getLine($diagram, line: $y));
+
+    $row[$x] === '.'
+        ? $row[$x] = 1
+        : $row[$x] ++;
+
+    $row[$x] !== 2 ?: $leastTwoLineOverlapCount ++;
+
+    $diagram = Read::putLine($diagram, line: $y, content: implode('', $row));
+}
+
+file_put_contents(__DIR__.'/diagram.txt', $diagram);
+
+dump($leastTwoLineOverlapCount);
---
+++ Y2021/D5/part_2.php
@@ -0,0 +1,115 @@
+<?php
+
+require __DIR__.'/../../vendor/autoload.php';
+require 'helpers.php';
+
+use Classes\Read;
+
+$input = Read::firstLine(__DIR__.'/puzzle.php');
+$puzzle = Read::lineByline(__DIR__.'/puzzle.php');
+unset($puzzle[0]);
+
+// separating bingoes
+$bingoes = [];
+$skipAfterKey = null;
+
+foreach ($puzzle as $i => $data) {
+    if ($skipAfterKey >= $i && $skipAfterKey) {
+        continue;
+    }
+    if (! trim($data)) {
+        continue;
+    }
+
+    // add complete bingo to data
+    for ($in = 1; $in < 5; $in++) {
+        $data .= ' '.$puzzle[$in + $i];
+    }
+
+    // to array
+    $data = array_filter(explode(' ', $data), fn ($val) => trim($val) !== '');
+    $d = array_values($data);
+
+    $bingoes[] = [
+        'rows' => [
+            $d[00] => false, $d[01] => false, $d[02] => false, $d[03] => false, $d[04] => false,
+            $d[05] => false, $d[06] => false, $d[07] => false, $d[8] => false, $d[9] => false,
+            $d[10] => false, $d[11] => false, $d[12] => false, $d[13] => false, $d[14] => false,
+            $d[15] => false, $d[16] => false, $d[17] => false, $d[18] => false, $d[19] => false,
+            $d[20] => false, $d[21] => false, $d[22] => false, $d[23] => false, $d[24] => false,
+        ],
+        'columns' => [
+            $d[00] => false, $d[05] => false, $d[10] => false, $d[15] => false, $d[20] => false,
+            $d[01] => false, $d[06] => false, $d[11] => false, $d[16] => false, $d[21] => false,
+            $d[02] => false, $d[07] => false, $d[12] => false, $d[17] => false, $d[22] => false,
+            $d[03] => false, $d[8] => false, $d[13] => false, $d[18] => false, $d[23] => false,
+            $d[04] => false, $d[9] => false, $d[14] => false, $d[19] => false, $d[24] => false,
+        ],
+        'lastDraw' => '',
+        'drawedNumbers' => [],
+    ];
+    $skipAfterKey = $i + 4;
+}
+
+// draw numbers
+$input = explode(',', $input);
+$solvedBingo = null;
+$lastSolved = null;
+
+foreach ($input as $inputKey => $number) {
+    for ($i = 0; $i < count($bingoes); $i++) {
+        if (isset($bingoes[$i])) {
+            drawBingo(by: $number, bingo: $bingoes[$i]);
+        }
+    }
+
+    // check is solved
+    foreach ($bingoes as $key => $bingo) {
+        if (is_bingo_solved($bingo)) {
+            $lastSolved = $bingo;
+            unset($bingoes[$key]);
+        }
+        if (count($bingoes) === 0) {
+            break 2;
+        }
+    }
+
+    $bingoes = array_values($bingoes);
+}
+
+$solvedBingo = $lastSolved;
+
+// calculate last solved bingo
+$sumUnmarkedNumbers = 0;
+
+foreach ($solvedBingo['rows'] as $number => $isDrawed) {
+    if (! $isDrawed) {
+        $sumUnmarkedNumbers += $number;
+    }
+}
+
+dump($sumUnmarkedNumbers * $solvedBingo['lastDraw']);
+
+// [
+//      [
+//          rows => [
+//              [22 => false, 13, 13, 1, 2
+//              22, 13, 13, 1, 2
+//              22, 13, 13, 1, 2
+//              22, 13, 13, 1, 2
+//              22, 13, 13, 1, 2]
+//          ]
+//          columns => [
+//              [22, 22, 22, 1, 2
+//              22, 13, 13, 1, 2
+//              22, 13, 13, 1, 2
+//              22, 13, 13, 1, 2
+//              22, 13, 13, 1, 2]
+//          ]
+//          lastDraw => ''
+//          drawedNumbers => []
+//      ]
+//      []
+//      []
+// ]
+//
