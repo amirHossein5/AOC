ddd6c5881ee98a78b3d0b1f6157ebdd9ba641ed6
amirHossein5 <adada5116@gmail.com>
2022-09-24 16:59:02 +0330

    Added command for creating new day

---
+++ src/Console/Commands/Make/Day.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AOC\Console\Commands\Make;
+
+use AOC\Console\Command;
+
+class Day extends Command
+{
+    protected string $signature = 'make:day {year} {day}';
+
+    protected string $description = 'Create a new AOC day.';
+
+    public function handle(): int
+    {
+        $dir = src_path().'/AOC/Y'.$this->argument('year').'/D'.$this->argument('day');
+        $dir = pathable($dir);
+
+        if (! is_dir($dir)) {
+            mkdir($dir, 0777, true);
+        }
+
+        collect(['part_1.php','part_2.php'])->each(fn ($file) =>
+            $this->createFile(filePath: pathable("{$dir}/{$file}"))
+        );
+
+        collect(['puzzle.txt'])->each(fn ($file) =>
+            $this->createFile(
+                filePath: pathable("{$dir}/{$file}"),
+                hasStub: false,
+            )
+        );
+
+        return Command::SUCCESS;
+    }
+
+    protected function getStub()
+    {
+        return stubs_path().'/AOCPart.stub';
+    }
+
+    private function createFile(string $filePath, bool $hasStub = true): void
+    {
+        $fileName = basename($filePath);
+
+        if (file_exists($filePath)) {
+            $this->error("File {$fileName} already exists.");
+            return;
+        }
+
+        if ($hasStub) {
+            file_put_contents($filePath, file_get_contents($this->getStub()));
+        } else {
+            file_put_contents($filePath, '');
+        }
+
+        $this->success("File {$fileName} Created successfully.");
+    }
+}
---
+++ tests/Feature/Commands/Make/AOCDayTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature\Commands\Make;
+
+use AOC\Services\Read;
+use Tests\TestCase;
+
+class AOCDayTest extends TestCase
+{
+    /** @var array<string, string> Value would be expected content. */
+    private array $expectedFiles = ['puzzle.txt' => '', 'part_1.php' => '', 'part_2.php' => ''];
+
+    public function test_creates_new_day()
+    {
+        $this->unlinkFilesForTesting[] = src_path().'/AOC/Y2021/D1/puzzle.txt';
+
+        $dir = src_path().'/AOC/Y2021/D1';
+        $dir = pathable($dir);
+
+        collect($this->expectedFiles)
+            ->except('puzzle.txt')
+            ->each(function ($key, $file) use ($dir) {
+                $this->assertFileExists($path = pathable($dir.'/'.$file));
+                $this->expectedFiles[$file] = file_get_contents($path);
+            });
+
+        $this->assertFileDoesNotExist(pathable($dir.'/'.'puzzle.txt'));
+
+        $this->command('php aoc make:day 2021 1');
+
+        collect($this->expectedFiles)->each(function ($key, $file) use ($dir) {
+            $this->assertFileExists($path = pathable($dir.'/'.$file));
+            $this->assertEquals(
+                file_get_contents($path),
+                $this->expectedFiles[$file]
+            );
+        });
+    }
+}
